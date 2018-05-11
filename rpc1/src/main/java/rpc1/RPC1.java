68a99449a8c5ca79d25527432276600fa2f78f19
James Castrejon <jamescastrejon12@gmail.com>
2018-05-11 10:36:11 -0600

    Added existing project

---
+++ rpc1/src/main/java/rpc1/RPC1.java
@@ -0,0 +1,19 @@
+package rpc1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@Configuration
+@EnableAutoConfiguration
+@ComponentScan
+@EnableJpaRepositories(basePackages="rpc1.repos")
+public class RPC1 extends SpringBootServletInitializer {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RPC1.class, args);
+	}
+}
---
+++ rpc1/src/main/java/rpc1/repos/ItemJpaRepository.java
@@ -0,0 +1,20 @@
+package rpc1.repos;
+
+import java.util.List;
+import java.util.Optional;
+
+import javax.persistence.LockModeType;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
+
+import rpc1.Item;
+
+public interface ItemJpaRepository extends JpaRepository<Item, Integer> {
+	
+	@Lock(LockModeType.PESSIMISTIC_WRITE)
+	Optional<Item> findById(Integer id);
+	
+	List<Item> findByNameLike(String name);
+	List<Item> findByCost(double cost);
+}
