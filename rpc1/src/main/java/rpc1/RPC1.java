14c6fbb0537e552315e71c66d375df2f94e3fb3e
James Castrejon <jamescastrejon12@gmail.com>
2018-05-14 10:07:18 -0600

    Added missing annotation

--- rpc1/src/main/java/rpc1/RPC1.java
+++ rpc1/src/main/java/rpc1/RPC1.java
@@ -2,6 +2,7 @@ package rpc1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
@@ -10,6 +11,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @Configuration
 @EnableAutoConfiguration
 @ComponentScan
+@EntityScan(basePackages="rpc1")
 @EnableJpaRepositories(basePackages="rpc1.repos")
 public class RPC1 extends SpringBootServletInitializer {
 
