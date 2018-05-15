661c2274d349defb95ff34ebf87e69c9d4900ec2
James Castrejon <jamescastrejon12@gmail.com>
2018-05-15 09:46:09 -0600

    Completed Category and added testing.

--- rpc1/src/main/java/rpc1/RestCategoryController.java
+++ rpc1/src/main/java/rpc1/RestCategoryController.java
@@ -1,5 +1,68 @@
 package rpc1;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/categories")
 public class RestCategoryController {
 
+	private List<Category> categories = new ArrayList<>();
+
+	@RequestMapping(method=RequestMethod.POST)
+	public void addCategory(
+			@RequestBody Category newC) {
+		categories.add(newC);
+	}
+	
+	@RequestMapping(method=RequestMethod.PUT)
+	public void updateCategory(
+			@RequestBody Category c) {
+		
+		Category existing = retrieveCategory(c.getId());
+		if(existing == null) {
+			throw new IllegalArgumentException("No existing Category");
+		}
+		existing.setId(c.getId());
+		existing.setName(c.getName());
+	}
+	
+	@RequestMapping(path="/{id}", method=RequestMethod.DELETE)
+	public void deleteCategory(
+			@PathVariable int id) {
+		categories.remove(id);
+	}
+	
+	@RequestMapping(path="/{id}", method=RequestMethod.GET)
+	public Category retrieveCategory(
+			@PathVariable int id) {
+		Category existing = categories.stream()
+				.filter(c -> c.getId() == id).findFirst().orElse(null);
+		return existing;
+	}
+	
+	@RequestMapping(method=RequestMethod.GET)
+	public List<Category> retrieveCategorys(
+			@RequestParam(required=false) Boolean sort) {
+		
+		if(sort == null) {
+			return categories;
+		}
+		
+		return categories.stream()
+			.sorted((l,r) -> {
+						return (sort ?
+								Long.compare(l.getId(), r.getId()) :
+								Long.compare(r.getId(), l.getId()));
+					})
+			.collect(Collectors.toList());
+	}
 }
--- rpc1/src/test/java/rpc1/Test_Category_End_Points.java
+++ rpc1/src/test/java/rpc1/Test_Category_End_Points.java
@@ -1,5 +1,64 @@
 package rpc1;
 
+import static com.jayway.restassured.RestAssured.given;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.junit.Test;
+
 public class Test_Category_End_Points {
+
+	@Test
+	public void basicPingTest(){
+		given().when().get("/categories")
+			.then().statusCode(200);
+	}
 	
+	@Test
+    public void basicCreateUser() {
+        Map<String,String> user = new HashMap<>();
+        user.put("id","1");
+        user.put("name", "Test");
+
+        given()
+        .contentType("application/json")
+        .body(user)
+        .when().post("/categories").then()
+        .statusCode(200);
+    }
+	
+	@Test
+    public void basicGetUsers() {
+        given().when().get("/categories")
+            .then().statusCode(200);
+    }
+
+	@Test
+	public void basicModifyUser() {
+		Map<String,String> user = new HashMap<>();
+        user.put("id","1");
+        user.put("name", "Testing");
+
+        given()
+	        .contentType("application/json")
+	        .body(user)
+	        .when().put("/categories").then()
+	        .statusCode(200);
+	}
+	
+	@Test
+    public void basicGetUserById() {
+		given().pathParam("id", 1)
+	    	.when().get("/categories/{id}")
+	        .then().statusCode(200);
+    }
+
+	@Test
+    public void basicDeleteItem() {
+		given().pathParam("id", 0)
+	        .when().delete("/categories/{id}")
+	        .then().statusCode(200);
+    }
+
 }
