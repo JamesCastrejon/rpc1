fb1834c5e15d12156a9ad7b59950d0225d993ab0
James Castrejon <jamescastrejon12@gmail.com>
2018-06-01 08:51:25 -0600

    Add other stuff

--- rpc1/src/main/java/rpc1/gui/Items.java
+++ rpc1/src/main/java/rpc1/gui/Items.java
@@ -4,12 +4,20 @@ import com.vaadin.server.VaadinRequest;
 import com.vaadin.spring.annotation.SpringUI;
 import com.vaadin.ui.Button;
 import com.vaadin.ui.UI;
+import com.vaadin.ui.VerticalLayout;
 
 @SpringUI(path="/items")
 public class Items extends UI {
 
+	VerticalLayout layoutV;
+	
+	public Items() {
+		layoutV = new VerticalLayout();
+	}
+	
 	@Override
 	protected void init(VaadinRequest request) {
 		setContent(new Button("Click Me!!!"));
 	}
+	
 }
